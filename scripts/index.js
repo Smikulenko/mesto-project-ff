c110f1a77247c9de0a52fa85923d43ba5e177c14
sofya Mikulenko <nephilim45@yandex.ru>
2024-12-10 15:43:32 +1000

    create add card and aelate card

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+// @todo: DOM узлы 
+const cardPlace = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+function createCard(data, removeCard){
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  const cardImage = cardElement.querySelector('.card__image');
+
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
+  cardElement.querySelector('.card__title').textContent = data.name;
+  deleteButton.addEventListener('click',removeCard);
+  return cardElement
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(evt){
+evt.target.closest('.places__item').remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function(data){
+  cardPlace.append(createCard(data, deleteCard));
+});
+
